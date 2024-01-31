8002710378254a5860c3aea8c2d7080fc153fda9
anajesset <106566783+anajesset@users.noreply.github.com>
2024-01-31 21:43:17 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
 	host: 'sql.freedb.tech',
 	dialect: 'mysql',
-    username: 'freedb_pakisenju',
-    password: 'Fpr&Wt2#Kaau*23',
-    database: 'freedb_sequelize-todo'
+    username: 'freedb_anajesset',
+    password: '2z*YJMyJp9GfEGq',
+    database: 'freedb_todolist_squalize'
 });
 
 module.exports = sequelize
